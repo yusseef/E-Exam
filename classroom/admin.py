ad16e7deef807e142a157f35d1039b16eed90fc8
passanthamdy <passant.hamdy99@gmail.com>
2020-06-15 17:13:45 +0200

    urls addition

--- classroom/admin.py
+++ classroom/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from classroom.models import User, Subject, Quiz, Question, Answer
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
